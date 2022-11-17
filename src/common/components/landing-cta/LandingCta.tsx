469b91da020b0fd68435cd13261c9cc4327b51b2
Dragos-Paul Strat <dragos1195@gmail.com>
2022-11-17 10:17:14 +0200

    feat: [C4R-988] CTA + other landing stuff

--- src/common/components/landing-cta/LandingCta.tsx
+++ src/common/components/landing-cta/LandingCta.tsx
@@ -9,13 +9,13 @@ const LandingCta = () => {
   const { t } = useTranslation('landing');
 
   return (
-    <div className="sm:pt-20 pt-10 grid md:grid-cols-2 grid-cols-1">
+    <div className="grid md:grid-cols-2 grid-cols-1">
       <LandingCtaItem
         backgroundColor="bg-yellow-500"
         title={t('cta.title_1')}
         description={t('cta.description_1')}
         buttonText={t('cta.register')}
-        buttonClasses="yellow-button bg-black text-white my-8 w-2/12"
+        buttonClasses="yellow-button bg-black text-white"
         onClick={() => openInNewTab(process.env.REACT_APP_CREATE_ONG_PROFILE_LINK!)}
       />
       <LandingCtaItem
@@ -23,7 +23,7 @@ const LandingCta = () => {
         title={t('cta.title_2')}
         description={t('cta.description_2')}
         buttonText={t('cta.discover_ongs')}
-        buttonClasses="yellow-button my-8 w-2/12"
+        buttonClasses="yellow-button"
         onClick={() => navigate('/organizations')}
         anchorLinkText={t('cta.p4g_link')}
       />
