aacdc94eef74eadd3ecc01b4410ff96b2b69de52
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-12-15 12:13:10 +0200

    fix: [C4R-1165] added max length to search field

--- src/common/components/search-field/SearchField.tsx
+++ src/common/components/search-field/SearchField.tsx
@@ -2,16 +2,17 @@ import { SearchIcon } from '@heroicons/react/solid';
 import React from 'react';
 import { classNames } from '../../helpers/Tailwind.helper';
 
-const SearchField = (props: {
-  config: Partial<any>;
-  readonly?: boolean;
-  disabled?: boolean;
-}) => {
+const SearchField = (props: { config: Partial<any>; readonly?: boolean; disabled?: boolean }) => {
   return (
     <div className="relative w-full">
       <div className="relative rounded-md">
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-          <SearchIcon className={classNames(`w-5 h-5`, props.config.defaultValue ? 'text-purple' : ' text-gray-500')} />
+          <SearchIcon
+            className={classNames(
+              `w-5 h-5`,
+              props.config.defaultValue ? 'text-purple' : ' text-gray-500',
+            )}
+          />
         </div>
         <input
           type={props.config.type}
@@ -20,7 +21,8 @@ const SearchField = (props: {
           onBlur={props.config.onBlur}
           className={classNames(
             props.config.error
-              ? 'border-red-300 text-red-900 placeholder-red-300 focus:ring-red-500 focus:border-red-500' : '',
+              ? 'border-red-300 text-red-900 placeholder-red-300 focus:ring-red-500 focus:border-red-500'
+              : '',
             props.config.addOn ? 'pl-10' : 'pl-4',
             'block h-14 w-full  shadow-md sm:text-lg text-gray-500 text-base disabled:bg-gray-100 p-4 font-titillium',
           )}
@@ -29,6 +31,7 @@ const SearchField = (props: {
           aria-invalid={props.config.error ? 'true' : 'false'}
           disabled={props.disabled}
           id={`${props.config.id}__input`}
+          maxLength={100}
         />
       </div>
     </div>
