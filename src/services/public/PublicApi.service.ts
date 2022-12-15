a0f82c05153c071131387a8d8a14e9766c01e8fe
OlteanuCosmin <dj.cosmin99@gmail.com>
2022-12-15 14:59:26 +0200

    refactor: [C4R-1156] updated API path for send contact form

--- src/services/public/PublicApi.service.ts
+++ src/services/public/PublicApi.service.ts
@@ -3,7 +3,5 @@ import { IContactMail } from '../../common/interfaces/ContactMail.interface';
 import API from '../API';
 
 export const sendContactMail = (contactMailPayload: IContactMail): Promise<void> => {
-  return API.post(`api/contact/feedback`, contactMailPayload).then(
-    (res: AxiosResponse<void>) => res.data,
-  );
+  return API.post(`api/contact`, contactMailPayload).then((res: AxiosResponse<void>) => res.data);
 };
