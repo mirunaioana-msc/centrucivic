376b6d6f8b2531baed7ee9027a4c9112ade03e7e
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2022-12-20 13:27:57 +0200

    fix: truncate selected values for multi select

--- src/common/components/select/Select.tsx
+++ src/common/components/select/Select.tsx
@@ -25,7 +25,9 @@ const Control = ({ children, ...props }: any) => {
     components.Control && (
       <components.Control {...props}>
         {props.icon && (
-          <props.icon className={classNames(`ml-1 w-5 h-5`, props.hasValue ? 'text-purple' : 'text-gray-500')} />
+          <props.icon
+            className={classNames(`ml-1 w-5 h-5`, props.hasValue ? 'text-purple' : 'text-gray-500')}
+          />
         )}
         {children}
       </components.Control>
@@ -34,7 +36,13 @@ const Control = ({ children, ...props }: any) => {
 };
 
 const MultiValue = ({ getValue, index }: any) =>
-  !index ? <p className='text-lg '>{getValue().length} {t('service_search:selected')}</p> : <p></p>;
+  !index ? (
+    <p className="text-lg truncate">
+      {getValue().length} {t('service_search:selected')}
+    </p>
+  ) : (
+    <p></p>
+  );
 
 const MultiSelect = ({
   placeholder,
@@ -48,11 +56,17 @@ const MultiSelect = ({
 }: MultiSelectConfig) => {
   const [defaultValue, setDefaultValue] = useState<any[]>([]);
 
-  useEffect(() => { setDefaultValue(value) }, [value])
+  useEffect(() => {
+    setDefaultValue(value);
+  }, [value]);
 
-  const components = { MultiValue, Control: (e: any) => Control({ ...e, icon }), DropdownIndicator: null };
+  const components = {
+    MultiValue,
+    Control: (e: any) => Control({ ...e, icon }),
+    DropdownIndicator: null,
+  };
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <Select
         placeholder={placeholder}
         classNamePrefix="reactselect"
