c52ad08fea3b8884606a62113549bc27ad125c29
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2024-05-21 12:09:04 +0300

    feat: udpate domains layout and fix domains display

--- src/pages/landing/components/domains/Domain.tsx
+++ src/pages/landing/components/domains/Domain.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import users from '../../../../assets/icons/users.svg';
+import stars from '../../../../assets/icons/stars.svg';
 import { MENU_ROUTES_HREF } from '../../../../common/constants/Menu.constants';
 
 interface DomainProps {
@@ -9,24 +8,17 @@ interface DomainProps {
 }
 
 const Domain = ({ name, id }: DomainProps) => {
-  const navigate = useNavigate();
-
   return (
     <a
       aria-label={name}
-      onClick={() => navigate(`${MENU_ROUTES_HREF.services}?domains=${id}`)}
+      href={
+        id === -1 ? `/${MENU_ROUTES_HREF.services}` : `/${MENU_ROUTES_HREF.services}?domains=${id}`
+      }
       className="text-black"
     >
-      <div className="bg-gray-100 hover:bg-yellow mx-auto max-w-2xl xs:aspect-square aspect-auto flex justify-center items-center flex-col gap-y-4 p-3 xs:h-auto h-full cursor-pointer">
-        <img
-          width={'69px'}
-          height={'88px'}
-          alt="domain image"
-          className="xl:max-w-[8rem] md:max-w-[3rem] xs:max-w-[2.8rem] max-w-[1.5rem]"
-          src={users}
-        ></img>
-
-        <p className="font-titilliumBold sm:text-2xl lg:text-3xl xs:text-md text-sm text-center">
+      <div className="flex gap-1">
+        <img width={24} height={24} alt="domain image" src={stars}></img>
+        <p className="font-titilliumBold md:text-xl lg:text-2xl text-md text-left hover:text-yellow">
           {name}
         </p>
       </div>
